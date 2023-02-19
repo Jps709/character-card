125dc70eecfe07f432d2db71c47e115f4e2f431b
Jps709 <jps7095@psu.edu>
2023-02-19 18:58:00 -0500

    Updoot

--- src/character-card.js
+++ src/character-card.js
@@ -1,6 +1,6 @@
 import { LitElement, html, css } from 'lit';
 const characterImg = new URL('../assets/obi.jpg', import.meta.url).href;
-
+import "@lrnwebcomponents/meme-maker/meme-maker.js";
 
 export class CharacterCard extends LitElement {
   static get properties(){
@@ -38,7 +38,9 @@ img{
 
 }
 @media screen and (max-width:500px) {
-  
+  details{
+    display:none;
+  }
 }
 .buttons {
   margin: 16px auto;
@@ -59,6 +61,8 @@ button:focus{
     this.description = 'Details';
     this.name='Some Nerd';
     this.title = 'Hello There';
+    this.top = 'Hello'
+    this.bottom = 'There'
   }
 
   render() {
@@ -66,7 +70,11 @@ button:focus{
 <div class="outline">
       <div class="lecard">
       <h2>${this.name}</h2>
-      <img src="${characterImg}"/>
+     <meme-maker image-url="${characterImg}"
+     top-text="${this.top}"
+     bottom-text="${this.bottom}">
+  </meme-maker>
+      
         <div class="header">
           <h4>${this.title}</h4>
         </div>
